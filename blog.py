705359345ed1a78725ec73394394792e5b5d8abd
githsem <53379599+githsem@users.noreply.github.com>
2020-09-27 00:48:46 +0200

    Update blog.py

--- blog.py
+++ blog.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
 @app.route("/")
 def index():
-    return render_template("index.html")
+    articles = [
+        {"id":1,"title":"Deneme1","content":"Deneme1 Icerik"},
+        {"id":2,"title":"Deneme2","content":"Deneme2 Icerik"},
+        {"id":3,"title":"Deneme2","content":"Deneme2 Icerik"}
+    ]
+    return render_template("index.html",articles = articles)
 
 @app.route("/about")
 def about():
