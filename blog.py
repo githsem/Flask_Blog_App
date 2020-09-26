34482a9f68d546a9be4dda2472318e0aea353bfa
githsem <53379599+githsem@users.noreply.github.com>
2020-09-27 01:05:41 +0200

    Update blog.py

--- blog.py
+++ blog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash, redirect, url_for, session, logging
+from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
 
 app = Flask(__name__)
 @app.route("/")
