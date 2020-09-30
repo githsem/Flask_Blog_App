90c34420fb5406a7e751860a71d5c4c74920ad3a
githsem <53379599+githsem@users.noreply.github.com>
2020-09-30 17:42:49 +0200

    Update blog.py

--- blog.py
+++ blog.py
@@ -6,8 +6,14 @@ from functools import wraps
 
 
 #Kullanici Giris Decorator'i
-
-
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if g.user is None:
+            return redirect(url_for('login', next=request.url))
+        return f(*args, **kwargs)
+    return decorated_function
+    
 #Kullanici Kayit Formu
 class RegisterForm(Form):
     name = StringField("Isim Soyisim", validators=[validators.length(min=4, max=25)])
